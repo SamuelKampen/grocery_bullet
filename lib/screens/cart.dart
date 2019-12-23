62ec960371dc8c61a646cfe0868521452aa71c70
scottkampen <scottykampen@gmail.com>
2019-12-22 17:10:45 -0800

    Add Google Pay

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_google_pay/flutter_google_pay.dart';
 import 'package:grocery_bullet/models/cart.dart';
 import 'package:grocery_bullet/models/item.dart';
 import 'package:intl/intl.dart';
@@ -10,12 +11,19 @@ import 'package:provider/provider.dart';
 // Used to format doubles as currency
 final oCcy = new NumberFormat("#,##0.00", "en_US");
 
-// TODO The code in this file is an unmaintainable disaster
+class Cart extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CartState();
+  }
+}
 
-class Cart extends StatelessWidget {
+class _CartState extends State<Cart> {
+  CartModel _cartModel;
+
   @override
   Widget build(BuildContext context) {
-    var cart = Provider.of<CartModel>(context);
+    _cartModel = Provider.of<CartModel>(context);
     return Container(
       color: Colors.white,
       child: Column(
@@ -32,7 +40,7 @@ class Cart extends StatelessWidget {
             width: double.infinity,
             child: RaisedButton(
               onPressed: () {
-                Map<Item, int> cartItems = cart.getCart();
+                Map<Item, int> cartItems = _cartModel.getCart();
                 for (Item item in cartItems.keys) {
                   int itemCountRequested = cartItems[item];
                   Firestore.instance.runTransaction((transaction) async {
@@ -48,9 +56,8 @@ class Cart extends StatelessWidget {
                     });
                   });
                 }
-                cart.resetCart();
-                Scaffold.of(context).showSnackBar(
-                    SnackBar(content: Text('Your items are on the way!!')));
+                _cartModel.resetCart();
+                _makeStripePayment();
               },
               child: Text('Buy', style: Theme.of(context).textTheme.display4),
               color: Colors.lightGreen,
@@ -60,6 +67,28 @@ class Cart extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  _makeStripePayment() async {
+    var environment = 'rest'; // or 'production'
+    if (!(await FlutterGooglePay.isAvailable(environment))) {
+      print('Google pay not available');
+    } else {
+      PaymentItem pm = PaymentItem(
+          stripeToken: 'pk_test_1IV5H8NyhgGYOeK6vYV3Qw8f',
+          stripeVersion: "2018-11-08",
+          currencyCode: "usd",
+          amount: "0.10",
+          gateway: 'stripe');
+
+      FlutterGooglePay.makePayment(pm).then((Result result) {
+        if (result.status == ResultStatus.SUCCESS) {
+          print('Success');
+        }
+      }).catchError((dynamic error) {
+        print(error.toString());
+      });
+    }
   }
 }
 
