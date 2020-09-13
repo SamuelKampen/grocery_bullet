3bfb7074b5cfd328d55e480d863f652b638859c4
scott <scottykampen@gmail.com>
2020-09-13 01:17:56 -0700

    Better sign in button.

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:grocery_bullet/common/Constants.dart';
 import 'package:grocery_bullet/screens/loading_page.dart';
 import 'package:grocery_bullet/services/AuthService.dart';
@@ -23,13 +24,13 @@ class SignInPage extends StatelessWidget {
             Container(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               alignment: Alignment.center,
-              child: RaisedButton(
+              child: SignInButton(
+                Buttons.Google,
                 onPressed: () async {
                   await AuthService.signInWithGoogle();
                   Navigator.pushReplacement(context,
                       MaterialPageRoute(builder: (context) => LoadingPage()));
                 },
-                child: const Text('Sign in with Google'),
               ),
             ),
           ],
